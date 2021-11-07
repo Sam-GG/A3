31442bb6cb51402c079a95961c80f946608ce883
Sam <samuelguttormson@gmail.com>
2021-11-06 23:04:36 -0600

    fleshed out basic shape toolbar class

--- src/main/java/com/example/a3/ShapeToolbar.java
+++ src/main/java/com/example/a3/ShapeToolbar.java
@@ -1,6 +1,16 @@
 package com.example.a3;
 
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 public class ShapeToolbar extends VBox {
+    public ShapeToolbar(){
+        Button rect = new Button("Rect");
+        Button square = new Button("Square");
+        Button circle = new Button("Circle");
+        Button oval = new Button("Oval");
+        Button line = new Button("Line");
+        this.setSpacing(5);
+        this.getChildren().addAll(rect, square, circle, oval, line);
+    }
 }
